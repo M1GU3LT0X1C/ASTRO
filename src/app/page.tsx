6ab5e3ee700e13e5ac59a7b6f4358a506e31f226
M1GU3LT0X1C <migueljarcanjo47@gmail.com>
2026-08-11 18:40:00 -0300

    feat: Finalizado a página home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import { Header } from "@/components/layout/Header"
 import { Footer } from "@/components/layout/Footer"
+import { HomeContent } from "@/components/home/HomeContent"
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="min-h- grid place-items-center">
-        <h1 className="text-4xl font-bold text-[#1E1B4B]">Protótipo</h1>
+      <main>
+        <HomeContent />
       </main>
       <Footer />
     </>
